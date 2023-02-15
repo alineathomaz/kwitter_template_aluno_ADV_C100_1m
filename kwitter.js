40713fe3c7a81c48c1ff523260480ac825b76c42
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2023-02-15 12:57:22 -0300

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,11 @@
+//função addUser
+function addUser() 
+{
+// Obtenha o nome do usuário da caixa de entrada e o armazene dentro de uma variável
+
+// Armazene o valor da variável user_name no armazenamento local com a chave user_name
+
+// Redirecione os usuários para kwitter_room.html.
+
+}
+
